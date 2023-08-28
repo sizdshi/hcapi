58fec5b6a1082baec076f43852852fe613731b2c
Alexshi <827571424@qq.com>
2023-08-28 16:05:31 +0800

    变更

---
+++ src/test/java/com/hc/apiproject/service/UserInterfaceInfoServiceTest.java
@@ -0,0 +1,26 @@
+package com.hc.apiproject.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+
+/**
+ * @创建人 Alexshi
+ * @创建时间 2023/8/17
+ * @描述
+ */
+@SpringBootTest
+public class UserInterfaceInfoServiceTest {
+
+    @Resource
+    private UserInterfaceInfoService userInterfaceInfoService;
+
+    @Test
+    public void invokeCount() {
+        boolean b = userInterfaceInfoService.invokeCount(1L, 1L);
+        Assertions.assertTrue(b);
+    }
+}
