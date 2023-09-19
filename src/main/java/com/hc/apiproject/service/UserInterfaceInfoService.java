5eedbc497594a9c04bfd5aa587025f339a48186a
Alexshi <827571424@qq.com>
2023-09-19 23:09:21 +0800

    前端页面加载成功，api商店界面成功

--- src/main/java/com/hc/apiproject/service/UserInterfaceInfoService.java
+++ src/main/java/com/hc/apiproject/service/UserInterfaceInfoService.java
@@ -1,7 +1,13 @@
 package com.hc.apiproject.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hc.hcapicommon.model.dto.userinterfaceinfo.UserInterfaceInfoQueryRequest;
 import com.hc.hcapicommon.model.entity.UserInterfaceInfo;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * @Author: Kenneth shi
@@ -18,4 +24,30 @@ public interface UserInterfaceInfoService extends IService<UserInterfaceInfo> {
      * @return
      */
     boolean invokeCount(long interfaceInfoId, long userId);
+
+    /**
+     * 获取查询条件
+     *
+     * @param interfaceInfoQueryRequest
+     * @return
+     */
+    QueryWrapper<UserInterfaceInfo> getQueryWrapper(UserInterfaceInfoQueryRequest interfaceInfoQueryRequest);
+
+
+    /**
+     * 分页获取接口信息封装
+     *
+     * @param userInterfaceInfoPage
+     * @param request
+     * @return
+     */
+    Page<UserInterfaceInfo> getUserInterfaceInfoVOPage(Page<UserInterfaceInfo> userInterfaceInfoPage, HttpServletRequest request);
+
+    /**
+     * 获取接口调用排名前 n 的接口信息
+     *
+     * @param limit 前几名
+     * @return List<InterfaceInfoVO>
+     */
+    List<UserInterfaceInfo> listTopInvokeInterfaceInfo(int limit);
 }
--- src/main/java/com/hc/apiproject/service/impl/inner/InnerUserInterfaceInfoServiceImpl.java
+++ src/main/java/com/hc/apiproject/service/impl/inner/InnerUserInterfaceInfoServiceImpl.java
@@ -2,6 +2,7 @@ package com.hc.apiproject.service.impl.inner;
 
 
 import com.hc.apiproject.service.UserInterfaceInfoService;
+import com.hc.hcapicommon.model.entity.UserInterfaceInfo;
 import com.hc.hcapicommon.service.InnerUserInterfaceInfoService;
 import org.apache.dubbo.config.annotation.DubboService;
 
@@ -19,5 +20,23 @@ public class InnerUserInterfaceInfoServiceImpl implements InnerUserInterfaceInfo
     @Override
     public boolean invokeCount(long interfaceInfoId, long userId) {
         return userInterfaceInfoService.invokeCount(interfaceInfoId, userId);
+    }
+
+    @Override
+    public UserInterfaceInfo hasLeftNum(Long interfaceId, Long userId) {
+        return userInterfaceInfoService.lambdaQuery()
+                .eq(UserInterfaceInfo::getInterfaceInfoId, interfaceId)
+                .eq(UserInterfaceInfo::getUserId, userId)
+                .one();
+    }
+
+    @Override
+    public Boolean addDefaultUserInterfaceInfo(Long interfaceId, Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserInterfaceInfo checkUserHasInterface(long interfaceId, long userId) {
+        return null;
     }
 }
